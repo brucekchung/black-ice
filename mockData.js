bbdf5e5cb0f0c6d18b22974a36536296f72d9286
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-04-08 14:01:27 -0600

    Add a mockData file

---
+++ mockData.js
@@ -0,0 +1,55 @@
+export const allData = [{
+    id: '1',
+    name: 'Base Camp',
+    date_collected: '08/19/18',
+    reflectance: '89%',
+    wavelength: '20um',
+    altitude: '4000m',
+    latlng: '1, -1',
+    region: 'Ishinca'
+    country: 'test'
+  }, {
+    id: '2',
+    name: 'Camp 1',
+    date_collected: '08/20/18',
+    reflectance: '70%',
+    wavelength: '110um',
+    altitude: '5000m',
+    latlng: '2, -2',
+    region: 'Pisco'
+    country: 'test2'       
+}]
+
+export const allLocations = [{
+    id: '1',
+    name: 'Base Camp',
+    lat: '1',
+    lng: '-1',
+    altitude: '4000m',
+    region: 'Ishinca',
+    country: 'Peru'    
+  }, {
+    id: '2',
+    name: 'Camp 1',
+    lat: '2',
+    lng: '-2',
+    altitude: '5000m',
+    region: 'Pisco',
+    country: 'Peru'
+}]
+
+export const data = [{
+    id: '1',
+    name: 'WP 1',
+    date_collected: '08/19/18',
+    reflectance: '89%',
+    wavelength: '20um',
+    location_id: '1'
+  }, {
+    id: '2',
+    name: 'WP 22',
+    date_collected: '08/20/18',
+    reflectance: '70%',
+    wavelength: '110um',
+    location_id: '2'
+}]
