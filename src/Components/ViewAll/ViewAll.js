46477266984779111a8c428310d243c9d4a64547
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-04-08 12:11:09 -0600

    Add search functionality to edit data page

--- src/Components/ViewAll/ViewAll.js
+++ src/Components/ViewAll/ViewAll.js
@@ -6,7 +6,7 @@ class ViewAll extends Component {
     super()
     this.state = {
       data: [{
-        id: 1,
+        id: '1',
         name: 'Col',
         date_collected: '08/19/18',
         reflectance: '89%',
@@ -14,7 +14,18 @@ class ViewAll extends Component {
         altitude: '4000m',
         latlng: '1, -1',
         country: 'test'
-      }]
+      }, {
+        id: '2',
+        name: 'Base',
+        date_collected: '08/18/18',
+        reflectance: '70%',
+        wavelength: 'something else',
+        altitude: '5000m',
+        latlng: '2, -2',
+        country: 'test2'       
+      }],
+      search: '',
+      filteredData: []
     }
   }
 
@@ -26,9 +37,11 @@ class ViewAll extends Component {
   // }
 
   renderData = () => {
-    return this.state.data.map(dataPoint => {
+    const data = this.state.filteredData.length > 0 ? this.state.filteredData : this.state.data
+
+    return data.map(dataPoint => {
       return (
-        <tr id={ dataPoint.id }>
+        <tr id={ dataPoint.id } key={ dataPoint.id }>
           <td>{ dataPoint.name }</td>
           <td>{ dataPoint.date_collected }</td>
           <td>{ dataPoint.reflectance }</td>
@@ -43,25 +56,44 @@ class ViewAll extends Component {
     })
   }
 
+  filterData = searchValue => {
+    return this.state.data.filter(dataPoint => {
+      const values = Object.values(dataPoint)
+
+      return values.find(value => value.includes(searchValue))
+    })
+  }
+
+  handleChange = e => {
+    const searchValue = e.target.value
+    const filteredData = this.filterData(searchValue)
+
+    this.setState({ filteredData })
+  }
 
   render() {
     return (
       <div>
         <Nav />
         <h2>All Data</h2>
+        <input type="text" name="search" onChange={ this.handleChange } /> 
         <table>
-          <tr>
-            <th>Name</th>
-            <th>Date Collected</th>
-            <th>Reflectance</th>
-            <th>Wavelength</th>
-            <th>Altitude</th>
-            <th>Lat/Lng</th>
-            <th>Country</th>
-            <th></th>
-            <th></th>
-          </tr>
-          { this.renderData() }
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date Collected</th>
+              <th>Reflectance</th>
+              <th>Wavelength</th>
+              <th>Altitude</th>
+              <th>Lat/Lng</th>
+              <th>Country</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.renderData() }
+          </tbody>
         </table>
       </div>
     )
