16dfd879d869597ff907d19c6af131d22d250241
Bruce Chung <brucekchung@gmail.com>
2018-04-09 13:28:18 -0600

    Add POST endpoint for locations

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
 
 server.set('port', process.env.PORT || 5000)
 
@@ -10,5 +13,22 @@ server.listen(server.get('port'), () => {
   console.log(`listening on ${server.get('port')}`)
 })
 
+server.get('/api/v1/locations/', (req, res) => {
+  res.status(200).send('works')
+})
+
+server.post('/api/v1/locations/', (req, res) => {
+  const { name } = req.body
+
+  if (!name) {
+    return res
+      .status(422)
+      .json('missing parameter')
+  }
+
+  database('locations').insert(req.body, 'id')
+    .then(item => res.status(200).json({ id: item[0], name}))
+    .catch(error => res.status(500).json({error}))
+})
 
 module.exports = server
