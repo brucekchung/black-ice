33f681d91282dc97efaf24c020727cf800ad1796
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-04-10 13:52:02 -0600

    Add ability to add a new location or choose existing, remove async/awaits for now

--- client/src/Components/DataEntry/DataEntry.js
+++ client/src/Components/DataEntry/DataEntry.js
@@ -11,10 +11,13 @@ class DataEntry extends Component {
     this.state = {
       allLocations: locationsOnly,
       selectedLocation: {
-        country: null,
-        coordinates: null,
-        region: null
+        country: '',
+        coordinates: '',
+        region: '',
+        latitude: '',
+        longitude: ''
       },
+      addLocation: false,
       data: [{
           name: '',
           date_collected: '',
@@ -24,13 +27,21 @@ class DataEntry extends Component {
     }
   }
 
-  componentDidMount = async () => {
-    const url = '/api/v1/locations'
+  // componentDidMount = () => {
+    // const url = '/api/v1/locations'
     // const allLocations = await apiCall(url);
 
     // this.setState({ 
     //   allLocations
     // })
+  // }
+
+  changeLocationForm = e => {
+    const text = e.target.innerText
+
+    e.target.innerText = text === 'Add a New Location' ? 'Choose an existing Location'
+                                                       : 'Add a New Location'
+    this.setState({ addLocation: !this.state.addLocation })
   }
 
   sortOptionsInfo = (locations, type) => {
@@ -55,20 +66,20 @@ class DataEntry extends Component {
     }, [])
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     const name = e.target.name
-    const value = e.target.options[e.target.selectedIndex].value
+    const value = e.target.options ? e.target.options[e.target.selectedIndex].value : e.target.value
     const selectedLocation = Object.assign(this.state.selectedLocation, { [name]: value })
 
     this.setState({ selectedLocation })
   }
 
-  handleClick = (e) => {
+  handleClick = e => {
     e.preventDefault();
     e.target.innerText === 'Add Data' ? this.addDataSet() : this.removeDataSet()
   }
 
-  updateData = (e) => {
+  updateData = e => {
     const name = e.target.name
     const value = e.target.value
     const index = e.target.parentNode.className
@@ -99,14 +110,14 @@ class DataEntry extends Component {
     }
   }
 
-  sendData = (data) => {
-    const url = '/api/v1/data'
+  sendData = ({ type, payload }) => {
+    const url = `/api/v1/${ type }`
     const init = { 
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(payload)
     }
 
     // apiCall(url, init)
@@ -120,14 +131,16 @@ class DataEntry extends Component {
     // return results.id
   }
 
-  handleSubmit = async e => {
-    e.preventDefault()
-
-    const location_id = await this.getLocationId();
-    const data = this.state.data.map(data => Object.assign(data, { location_id }))
-    
-    await this.sendData(data)
-    this.setState({ 
+  resetState = () => {
+    this.setState({
+      selectedLocation: {
+        country: '',
+        coordinates: '',
+        region: '',
+        latitude: '',
+        longitude: ''
+      },
+      addLocation: false,
       data: [{
           name: '',
           date_collected: '',
@@ -135,6 +148,35 @@ class DataEntry extends Component {
           wavelength: ''
       }]
     })
+  }
+
+  submit = () => {
+    const location_id = this.getLocationId();
+    const data = this.state.data.map(data => Object.assign(data, { location_id }))
+
+    this.submitData()    
+  }
+
+  submitWithLocation = () => {
+    const { country, region, latitude, longitude } = this.state.selectedLocation
+    const location = { 
+      type: 'Location',
+      payload: { country, region, latitude, longitude }
+    }
+    const data = {
+      type: 'Data',
+      payload: this.state.data
+    }
+    const createdLocation =  this.sendData(location)
+    const createdData = this.sendData(data)
+
+    this.resetState()
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    return this.state.addLocation === false ? this.submit() : this.submitWithLocation()    
   }
 
   availableOptions = type => {
@@ -155,15 +197,48 @@ class DataEntry extends Component {
         <button>Import CSV</button>
         <form onSubmit={ this.handleSubmit }>
           <h3>Location</h3>
-          <DropDown name='country'
-                    options={ this.sortOptionsInfo(this.state.allLocations, 'country') }
-                    handleChange={ this.handleChange } />
-          <DropDown name='region'
-                    options={ this.availableOptions('region') }
-                    handleChange={ this.handleChange } />
-          <DropDown name='coordinates'
-                    options={ this.availableOptions('coordinates') }
-                    handleChange={ this.handleChange } />
+          {
+            !this.state.addLocation &&
+            <div className="choose-location">
+              <h4>Choose an existing Location</h4>
+              <DropDown name='country'
+                        options={ this.sortOptionsInfo(this.state.allLocations, 'country') }
+                        handleChange={ this.handleChange } />
+              <DropDown name='region'
+                        options={ this.availableOptions('region') }
+                        handleChange={ this.handleChange } />
+              <DropDown name='coordinates'
+                        options={ this.availableOptions('coordinates') }
+                        handleChange={ this.handleChange } />
+            </div>
+          }
+          {
+            this.state.addLocation &&
+            <div className="add-location">
+              <h4>Add a New Location</h4>
+              <input type="text"
+                     name="country"
+                     placeholder="Country"
+                     value={ this.state.selectedLocation.country }
+                     onChange={ this.handleChange }/>
+              <input type="text"
+                     name="region"
+                     placeholder="Region"
+                     value={ this.state.selectedLocation.region }
+                     onChange={ this.handleChange }/>
+              <input type="text"
+                     name="latitude"
+                     placeholder="Latitude"
+                     value={ this.state.selectedLocation.latitude }
+                     onChange={ this.handleChange }/>
+              <input type="text"
+                     name="longitude"
+                     placeholder="Longitude"
+                     value={ this.state.selectedLocation.longitude }
+                     onChange={ this.handleChange }/>
+            </div>
+          }
+          <button type="button" onClick={ this.changeLocationForm }>Add a New Location</button>
           <h3>Data</h3>
           <DataForm data={ this.state.data }
                     handleChange={ this.updateData }
