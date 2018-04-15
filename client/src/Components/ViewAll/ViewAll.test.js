ee831fad0867bf4e100f36ad0bf6b7dc240150e2
Bruce Chung <brucekchung@gmail.com>
2018-04-15 15:58:48 -0600

    Begin view-all tests

---
+++ client/src/Components/ViewAll/ViewAll.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ViewAll from './ViewAll'
+import { shallow } from 'enzyme'
+
+describe('ViewAll', () => {
+  // it('should match snapshot', () => {
+  //   const wrapper = shallow(<ViewAll />)
+
+  //   expect(wrapper).toMatchSnapshot()
+  // })
+
+  it('should exist', () => {
+
+  })
+})
