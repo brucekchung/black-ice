a3a90234347d3b242dc929ace90596d92ce8ee41
Bruce Chung <brucekchung@gmail.com>
2018-04-15 17:16:09 -0600

    Pseudocode tests

--- client/src/Components/ViewAll/ViewAll.test.js
+++ client/src/Components/ViewAll/ViewAll.test.js
@@ -12,4 +12,55 @@ describe('ViewAll', () => {
   it('should match snapshot upon load', () => {
     expect(wrapper).toMatchSnapshot()
   })
+
+  it('should have a default state', () => {
+    const expected = {
+      data: [],
+      filteredData: [],
+      editableContent: null
+    }
+    const actual = wrapper.state()
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should make an apiCall on componentDidMount', () => {
+
+  })
+
+  it('should setState on componentDidMount', () => {
+
+  })
+
+  it('should setState when editData button is clicked', () => {
+    // const e = {
+    //   target: {
+    //     innerText: 'Add Data'
+    //   },
+    //   preventDefault: jest.fn()
+    // }
+
+    expect(wrapper.state('editableContent')).toEqual(null)
+    // console.log(wrapper.debug())
+    // wrapper.instance().handleClick(e)
+    // expect(wrapper.state('data').length).toEqual(2)
+
+  })
+
+  it('should make an apiCall when updateData is run', () => {
+
+  })
+
+  it('should setState when saveData is called', () => {
+
+  })
+
+  it('should setState when deleteRow is called', () => {
+
+  })
+
+  it('should setState with the filtered data on handleChange', () => {
+
+  })
+
 })
