79b6095e2446be894e18cef67049c8183420c470
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-04-17 15:14:45 -0600

    Add styling to reports page

--- client/src/Components/Reports/Reports.js
+++ client/src/Components/Reports/Reports.js
@@ -142,38 +142,46 @@ class Reports extends Component {
       <div className="Reports">
         <div className="dashboard">
           <div className="total-samples">
-            <h4>TOTAL</h4>
+            <h4>Total</h4>
             <h3>{ this.state.allSamples.length }</h3>
-            <h4>SAMPLES</h4>
+            <h4>Samples</h4>
           </div>
           <div className="total-locations">
-            <h4>TOTAL</h4>
+            <h4>Total</h4>
             <h3>{ this.state.allLocations.length }</h3>
-            <h4>LOCATIONS</h4>
+            <h4>Locations</h4>
           </div>
           <div className="avg-samples">
-            <h4>AVERAGE</h4>
+            <h4>Average</h4>
             <h3>{ this.calculateAverage() }</h3>
-            <h4>SAMPLES/LOCATION</h4>
+            <h4>Samples / Location</h4>
           </div>
           <div className="avg-reflectance">
-            <h4>AVERAGE</h4>
+            <h4>Average</h4>
             <h3>{ this.calculateReflectance() }</h3>
-            <h4>REFLECTANCE</h4>
+            <h4>Reflectance</h4>
           </div>
         </div>
         <form onSubmit={ this.handleSubmit }>
-          <DropDown name='country'
-                    options={ this.removeRepeatOptions(this.state.allLocations, 'country') }
-                    handleChange={ this.handleChange } />
-          <DropDown name='region'
-                    options={ this.availableOptions('region') }
-                    handleChange={ this.handleChange } />
-          <DropDown name='coordinates'
-                    options={ this.availableOptions('coordinates') }
-                    handleChange={ this.handleChange } />
-          <input id="startDate" type="text" name="startDate" placeholder="Start Date" value={ this.state.startDate } onChange={ this.setDates }/>
-          <input id="endDate" type="text" name="endDate" placeholder="End Date" value={ this.state.endDate } onChange={ this.setDates }/>
+          <h3>Choose a location:</h3>
+          <div className="choose-location">
+            <DropDown name='country'
+                      options={ this.removeRepeatOptions(this.state.allLocations, 'country') }
+                      handleChange={ this.handleChange } />
+            <DropDown name='region'
+                      options={ this.availableOptions('region') }
+                      handleChange={ this.handleChange } />
+            <DropDown name='coordinates'
+                      options={ this.availableOptions('coordinates') }
+                      handleChange={ this.handleChange } />
+          </div>
+          <h3>Choose a date range:</h3>
+          <div className="choose-dates">
+            <label htmlFor="startDate">Start</label>
+            <input id="startDate" type="text" name="startDate" placeholder="mm/dd/yy" value={ this.state.startDate } onChange={ this.setDates }/>
+            <label htmlFor="endDate">End</label>
+            <input id="endDate" type="text" name="endDate" placeholder="mm/dd/yy" value={ this.state.endDate } onChange={ this.setDates }/>
+          </div>
           <button type='submit'>Generate Report</button>
         </form>
         {
