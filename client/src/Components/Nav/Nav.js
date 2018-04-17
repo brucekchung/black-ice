a39208c7aabbd1a3dd273cad54985ed5c682c795
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-04-17 11:20:53 -0600

    Add basic styling to inputs, select elements, buttons and headers

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -5,9 +5,9 @@ import './Nav.css'
 const Nav = () => (
   <div className='Nav-bar'>
     <NavLink className="Nav" activeClassName='selected' exact to='/'>Home</NavLink>
-    <NavLink className="Nav" activeClassName='selected' to='/DataEntry'>DataEntry</NavLink>
     <NavLink className="Nav" activeClassName='selected' to='/Reports'>Reports</NavLink>
-    <NavLink className="Nav" activeClassName='selected' to='/ViewAll'>EditData</NavLink>
+    <NavLink className="Nav" activeClassName='selected' to='/DataEntry'>Add Data</NavLink>
+    <NavLink className="Nav" activeClassName='selected' to='/ViewAll'>Edit Data</NavLink>
   </div>
 )
 
