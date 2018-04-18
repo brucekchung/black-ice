0445bf8a98048670fc7334b3e211b5d8f91ca1e9
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-04-18 08:28:39 -0600

    Fix linting errors

--- client/src/Components/DataEntry/DataEntry.js
+++ client/src/Components/DataEntry/DataEntry.js
@@ -84,9 +84,6 @@ class DataEntry extends Component {
     const index = parseInt(e.target.parentNode.className.replace('data-form-set ', ''), 10)
     const sampleData = [...this.state.sampleData]
 
-    console.log('parentNode: ', e.target.parentNode)
-    console.log('index: ', index)
-
     sampleData[index][name] = value
     this.setState({ sampleData })
   }
